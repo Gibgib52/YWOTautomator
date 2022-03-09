f033227cb3d7eb6672eb65f8c9b84aa8094c3504
Gibson Hall <gibsonjhall@gmail.com>
2022-03-09 12:21:35 -0600

    first commit

---
+++ YWOTmouseMover.py
@@ -0,0 +1,25 @@
+"""
+    Moves mouse to scroll for you in your world of text
+"""
+
+import mouse
+import time
+
+sWidth = 1920
+sHeight = 1080
+
+
+def scroll(direction):
+
+    if(direction == "n"):
+        pass
+    elif(direction == "e"):
+        mouse.drag(sWidth/2, sHeight/2, (sWidth/2)+500, sHeight/2, absolute=True, duration=0.01)
+    elif(direction == "s"):
+        pass
+    elif(direction == "w"):
+        pass
+
+mouse.on_middle_click(scroll, args=("e"))
+while True:
+    time.sleep(0.025)
